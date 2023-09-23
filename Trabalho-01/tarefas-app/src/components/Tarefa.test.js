8d755d8fec7802d89146d876e5e5b289458d7c7c
Livia <livia.jacklinne@gec.inatel.br>
2023-09-23 09:51:49 -0300

    Trabalho-01

--- Trabalho-01/tarefas-app/src/components/Tarefa.test.js
+++ Trabalho-01/tarefas-app/src/components/Tarefa.test.js
@@ -1,73 +1,73 @@
-// // Tarefa.test.js
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import Tarefa from './Tarefa';
+// Tarefa.test.js
+import { render, screen, fireEvent } from '@testing-library/react';
+import Tarefa from './Tarefa';
 
-// test('renderiza uma tarefa', () => {
-//   const tarefa = {
-//     titulo: 'Título da Tarefa',
-//     descricao: 'Descrição da Tarefa',
-//     concluida: false,
-//   };
+test('renderiza uma tarefa', () => {
+  const tarefa = {
+    titulo: 'Título da Tarefa',
+    descricao: 'Descrição da Tarefa',
+    concluida: false,
+  };
 
-//   render(<Tarefa tarefa={tarefa} />);
+  render(<Tarefa tarefa={tarefa} />);
 
-//   expect(screen.getByText('Título da Tarefa')).toBeInTheDocument();
-//   expect(screen.getByText('Descrição da Tarefa')).toBeInTheDocument();
-// });
+  expect(screen.getByText('Título da Tarefa')).toBeInTheDocument();
+  expect(screen.getByText('Descrição da Tarefa')).toBeInTheDocument();
+});
 
-// test('edita uma tarefa', () => {
-//   const tarefa = {
-//     titulo: 'Título Original',
-//     descricao: 'Descrição Original',
-//     concluida: false,
-//   };
+test('edita uma tarefa', () => {
+  const tarefa = {
+    titulo: 'Título Original',
+    descricao: 'Descrição Original',
+    concluida: false,
+  };
 
-//   render(<Tarefa tarefa={tarefa} />);
+  render(<Tarefa tarefa={tarefa} />);
 
-//   const editarButton = screen.getByText('Editar');
-//   fireEvent.click(editarButton);
+  const editarButton = screen.getByText('Editar');
+  fireEvent.click(editarButton);
 
-//   const novoTituloInput = screen.getByPlaceholderText('Título');
-//   const novaDescricaoInput = screen.getByPlaceholderText('Descrição');
-//   const salvarButton = screen.getByText('Salvar');
+  const novoTituloInput = screen.getByPlaceholderText('Título');
+  const novaDescricaoInput = screen.getByPlaceholderText('Descrição');
+  const salvarButton = screen.getByText('Salvar');
 
-//   fireEvent.change(novoTituloInput, { target: { value: 'Título Editado' } });
-//   fireEvent.change(novaDescricaoInput, { target: { value: 'Descrição Editada' } });
+  fireEvent.change(novoTituloInput, { target: { value: 'Título Editado' } });
+  fireEvent.change(novaDescricaoInput, { target: { value: 'Descrição Editada' } });
 
-//   fireEvent.click(salvarButton);
+  fireEvent.click(salvarButton);
 
-//   expect(screen.getByText('Título Editado')).toBeInTheDocument();
-//   expect(screen.getByText('Descrição Editada')).toBeInTheDocument();
-// });
+  expect(screen.getByText('Título Editado')).toBeInTheDocument();
+  expect(screen.getByText('Descrição Editada')).toBeInTheDocument();
+});
 
-// test('conclui uma tarefa', () => {
-//   const tarefa = {
-//     titulo: 'Título da Tarefa',
-//     descricao: 'Descrição da Tarefa',
-//     concluida: false,
-//   };
+test('conclui uma tarefa', () => {
+  const tarefa = {
+    titulo: 'Título da Tarefa',
+    descricao: 'Descrição da Tarefa',
+    concluida: false,
+  };
 
-//   render(<Tarefa tarefa={tarefa} />);
+  render(<Tarefa tarefa={tarefa} />);
 
-//   const concluirButton = screen.getByText('Concluir');
-//   fireEvent.click(concluirButton);
+  const concluirButton = screen.getByText('Concluir');
+  fireEvent.click(concluirButton);
 
-//   expect(screen.getByText('Desmarcar como Concluída')).toBeInTheDocument();
-// });
+  expect(screen.getByText('Desmarcar como Concluída')).toBeInTheDocument();
+});
 
-// test('exclui uma tarefa', () => {
-//   const tarefa = {
-//     titulo: 'Título da Tarefa',
-//     descricao: 'Descrição da Tarefa',
-//     concluida: false,
-//   };
+test('exclui uma tarefa', () => {
+  const tarefa = {
+    titulo: 'Título da Tarefa',
+    descricao: 'Descrição da Tarefa',
+    concluida: false,
+  };
 
-//   const mockExcluirTarefa = jest.fn();
+  const mockExcluirTarefa = jest.fn();
 
-//   render(<Tarefa tarefa={tarefa} excluirTarefa={mockExcluirTarefa} />);
+  render(<Tarefa tarefa={tarefa} excluirTarefa={mockExcluirTarefa} />);
 
-//   const excluirButton = screen.getByText('Excluir');
-//   fireEvent.click(excluirButton);
+  const excluirButton = screen.getByText('Excluir');
+  fireEvent.click(excluirButton);
 
-//   expect(mockExcluirTarefa).toHaveBeenCalledTimes(1);
-// });
+  expect(mockExcluirTarefa).toHaveBeenCalledTimes(1);
+});
